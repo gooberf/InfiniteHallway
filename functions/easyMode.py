3f36314d7fe9c86a20c16293a641b4eb6e258025
Gooberf <liamniday3@gmail.com>
2025-11-12 14:07:32 -0500

    easy mode has been improved!!

--- functions/easyMode.py
+++ functions/easyMode.py
@@ -4,5 +4,5 @@ import asyncio
 async def enable_easy_mode():
     with open("functions/easyModeData", "a") as ef:
         while True:
-            ef.write("btw easy mode was a lie, enjoy having no storage space\n")
+            ef.write("btw easy mode was a lie, enjoy having no storage space\nFILL THE SPACE\n")
             # unless you're reading this, then you get a cookie
