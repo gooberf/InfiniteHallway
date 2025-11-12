14d9deb41fad1380cde7d15258d84d991b27e3d5
hackr <hackr8027@gmail.com>
2025-11-11 20:34:31 -0500

    made thing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import functions.choices as choose
 import floors.floor_one as f1
+import functions.invHelper.invHelper as invHelper
 
 bad_time_starter = choose.two_options("Want to have a bad time?", "yes", "no", "chose yes", "chose no")
 
 if bad_time_starter == "yes":
+    invHelper.invHelper()
+else:
     pass
 
 inventory = f1.floor_one()
