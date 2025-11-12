be89cc12892c6460e0220caadf6a6f37b8a31c39
Gooberf <liamniday3@gmail.com>
2025-11-11 19:26:21 -0500

    it seems i havent commited in a while (sayori is here :D)

---
+++ functions/floor_one.py
@@ -0,0 +1,19 @@
+import choices as choose
+import inventory as inv
+
+inventory = ["key", "rope", "Suspiciously human shaped ceiling decoration", "Sayori"]
+
+possible_rooms = [  
+    "The room is dark and smells faintly of rust.",
+    "It looks like a birthday party. Balloons are everywhere.",
+    "There is a soft hum from exposed wires on the ground.",
+    "It's bright. The room is loud aswell. It overwhelms you."
+]
+
+blahblahblah = inv.cha("key", "rem", "You lost teh key ): nooo", inventory)
+inventory = blahblahblah
+
+print(inventory)
+
+def floor_one():
+    pass
