deb3c4557c8b38ec2726f43858e1476f6d1b67b2
Gooberf <liamniday3@gmail.com>
2025-11-11 20:12:05 -0500

    gay

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
 import functions.choices as choose
-'''
-this is how to format the two choice thing
-
-choice = choose.two_options("Left or right? ", "left", "right", "You go left", "You go right", "Try again! Error has occured.")
-print(f"choice returned: {choice}")
-a
-'''
-
