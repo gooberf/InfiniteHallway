de0f3ceacc0f2a8e4d5849dbb4abfc5fea3dd053
Gooberf <liamniday3@gmail.com>
2025-11-11 23:45:24 -0500

    Added a little bit of the playable game. Room Index 0 and Room Index 1 both have full functionality now.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import functions.choices as choose
 import floors.floor_one as f1
 import os
-"""
+
 if os.name == 'nt':
     os.system('cls')
 else:
     os.system('clear')
-"""
+# this runs the game itself
 inventory = f1.floor_one()
