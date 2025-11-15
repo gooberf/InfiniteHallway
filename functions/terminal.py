21499a6ec46414bcce8a16b69c5b29b7a004bba7
hackr <hackr8027@gmail.com>
2025-11-15 17:06:06 -0500

    made traceback generation for possible 4th wall breaking moments

--- functions/terminal.py
+++ functions/terminal.py
@@ -1,4 +1,8 @@
 import os
+import rich
+from rich.console import Console
+
+console = Console()
 
 def clear():
     """Clear the terminal screen. Works on both Windows and Unix systems."""
@@ -10,3 +14,10 @@ def clear():
 def run(cmd):
     os.system(cmd)
     
+def genTraceback(msg):
+    raise ValueError(msg)
+
+def traceback(msg):
+    try: genTraceback(msg)
+    except:
+        console.print_exception(show_locals=True)
