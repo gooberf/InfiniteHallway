dd581d5b7c51c43202b6b0403061d505e7169e6d
hackr <hackr8027@gmail.com>
2025-11-14 19:47:34 -0500

    revert to old save.py

--- functions/save.py
+++ functions/save.py
@@ -1,24 +1,11 @@
 import json
 import os
-import functions.choices as choose
 
 def save(data):
-    jsonData = {
-        'inventory': data['inventory'],
-        'bought_key': data['bought_key'],
-        'door_open': data['door_open']
-    }
-    
-    #if not os.path.exists('saves'):
-    #    os.makedirs('saves')
-    while True:
-        try:
-            with open('saves/save.json', 'w') as f:
-                if not os.path.exists('saves/save.json'):
-                    os.mkdir('saves')
-                json.dump(data, f)
-        except KeyboardInterrupt:
-            choose.two_options("You're still saving! Are you sure you want to quit?", "yes", "no")
+    if not os.path.exists('saves'):
+        os.makedirs('saves')
+    with open('saves/save.json', 'w') as f:
+        json.dump(data, f)
 
 def load():
     try:
